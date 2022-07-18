6ac94705305ef7cd41c0161afbbf1b35f0323125
nguyentr02 <nguyen.truong200226@vnuk.edu.vn>
2022-07-18 15:46:13 +0700

    Edit Navbar for leftNav

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -4,9 +4,8 @@ import './NavbarElement.css';
 const Navbar = () => {
   return (
     <div class="Navbar">
-      <div class="leftNav">
+      {/* <div class="leftNav">
         <a id="trang-chu-bt" class="btn" href="/trang-chu.js">Trang chủ</a>
-        {/* <a id="gioi-thieu-bt" href="/gioi-thieu.html">Về VNUK</a> */}
         <div class="dropdown">
           <a class="dropbtn" href='/gioi-thieu.js'>Về VNUK</a>
           <div class="dropdown-content">
@@ -25,7 +24,7 @@ const Navbar = () => {
                 <a tabIndex={-1} href="#">Quà lưu niệm</a>
               </div>
             </div>
-            {/* <div class="dropdown-content1">
+            <div class="dropdown-content1">
               <a href="#">Lịch sử phát triển</a>
               <a href="#">Tầm nhìn & sứ mệnh</a>
               <a href="#">Cơ cấu tổ chức</a>
@@ -34,7 +33,7 @@ const Navbar = () => {
               <a href="#">Ba công khai</a>
               <a href="#">Liên hệ</a>
               <a href="#">Quà lưu niệm</a>
-            </div> */}
+            </div>
 
             <a href="#">Đào tạo</a>
             <a href="#">Tuyển sinh</a>
@@ -45,6 +44,46 @@ const Navbar = () => {
 
           </div>
         </div>
+      </div> */}
+
+      <div class='leftNav'>
+        <ul>
+          <li id="trang-chu-bt"><a href="/trang-chu.js">Trang chủ</a></li>
+          <li class="dropbtn"><a href="/gioi-thieu.js">Về VNUK</a>
+            <div className='submenu1'>
+              <ul>
+                <li className='hoverme'><a href='/gioi-thieu.js'>Giới thiệu</a>
+                  <div className='submenu2'>
+                    <ul>
+                      {/* <li><a href='#'></a>Lịch sử phát triển</li> */}
+                      <li><a href="#">Tầm nhìn & sứ mệnh</a>
+                      </li>
+                      <li><a href='#'>Lịch sử phát triển</a></li>
+                      <li><a href="#">Cơ cấu tổ chứch</a>
+                      </li>
+                      <li><a href="#">Thành tựu</a></li>
+                      <li><a href="#">Đối tác</a></li>
+                      <li><a href="#">Ba công khai</a></li>
+                      <li><a href="#">Liên hệ</a></li>
+                      <li><a href="#">Quà lưu niệm</a></li>
+                    </ul>
+                  </div>
+                </li>
+                <li><a href="#">Đào tạo</a>
+                </li>
+                <li><a href="#">Tuyển sinh</a>
+                </li>
+                <li><a href="#">Sinh viên</a></li>
+                <li><a href="#">Nghiên cứu</a></li>
+                <li><a href="#">Sáng kiến</a></li>
+                <li><a href="#">Tin tức & sự kiện</a></li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div className='rightNav'>
+        
       </div>
     </div>
   )
