d6953a43f1d7511dd4537bba3ddd59e0ee97e718
MinhHung2703 <nguyenminhhung27032002@gmail.com>
2022-07-19 18:25:08 +0700

    trang gioi thieu

---
+++ src/pages/gioi-thieu.js
@@ -0,0 +1,61 @@
+import React from "react";
+import './gioi-thieu.css'
+
+const Gioithieu = () => {
+    return (
+        <div className="GioiThieu">
+            <div className="background">
+                <div className="both-img12">
+                    <img className="img1" src="https://scontent.fhan3-1.fna.fbcdn.net/v/t1.6435-9/131382005_3341946682601404_3161856688440634496_n.jpg?stp=dst-jpg_s960x960&_nc_cat=102&ccb=1-7&_nc_sid=8631f5&_nc_ohc=xTERtlajUtgAX9y0425&_nc_ht=scontent.fhan3-1.fna&oh=00_AT8i88BNZoZ9Mq9otj4SDpmoEcjJ4kcib4yKgFXENlyzjw&oe=62FC19D9" />
+                    <img className="img2" src="https://scontent.fhan3-1.fna.fbcdn.net/v/t1.6435-9/131382005_3341946682601404_3161856688440634496_n.jpg?stp=dst-jpg_s960x960&_nc_cat=102&ccb=1-7&_nc_sid=8631f5&_nc_ohc=xTERtlajUtgAX9y0425&_nc_ht=scontent.fhan3-1.fna&oh=00_AT8i88BNZoZ9Mq9otj4SDpmoEcjJ4kcib4yKgFXENlyzjw&oe=62FC19D9" />
+                    <img className="vnuk" src="/public/img/vnuk.png" />
+                </div>
+
+                <div class="column-layout">
+                    <div class="column-item siderbar-one">
+                        <img className="sach" src="../../public/img/sách.png"/>
+                        <p>Students' reading achievements are typically evaluated by the target ability as well as a secondary
+                            ability to understand the content of the passage.Students' reading achievements are typically evaluated
+                            by the target ability as well as a secondary ability to understand the content of the passage.</p>
+                    </div>
+                    <div class="column-item main-column">
+                        <img className="bang" src="../../public/img/bằng.png"/>
+                        <p>Students' reading achievements are typically evaluated by the target ability as well as a secondary
+                            ability to understand the content of the passage.Students' reading achievements are typically evaluated
+                            by the target ability as well as a secondary ability to understand the content of the passage.Students'
+                            reading achievements are typically evaluated by the target ability as well as a secondary ability to
+                            understand the content of the passage.</p>
+                        <p>Students' reading achievements are typically evaluated by the target ability as well as a secondary
+                            ability to understand the content of the passage.Students' reading achievements are typically evaluated
+                            by the target ability as well as a secondary ability to understand the content of the passage.</p>
+                        <p>Students' reading achievements are typically evaluated by the target ability as well as a secondary
+                            ability to understand the content of the passage.Students' reading achievements are typically evaluated
+                            by the target ability as well as a secondary ability to understand the content of the passage.</p>
+                    </div>
+                    <div class="column-item siderbar-two">
+                        <img className="truong" src="../../public/img/trường.png"/>
+                        <p>Students' reading achievements are typically evaluated by the target ability as well as a secondary
+                            ability to understand the content of the passage.Students' reading achievements are typically evaluated
+                            by the target ability as well as a secondary ability to understand the content of the passage.</p>
+                    </div>
+                </div>
+
+                {/* <div class="feature-layout">
+                    <div class="feature-item">
+                        <img className="sach" src="../../public/img/sách.png"/>
+                        <p>Students' reading achievements are typically evaluated by the target ability as well as a secondary ability to understand the content of the passage.Students' reading achievements are typically evaluated by the target ability as well as a secondary ability to understand the content of the passage.Students' reading achievements are typically evaluated by the target ability as well as a secondary ability to understand the content of the passage.</p>
+                    </div>
+                    <div class="feature-item">
+                        <img className="bang" src="../../public/img/bằng.png"/>
+                        <p>Students' reading achievements are typically evaluated by the target ability as well as a secondary ability to understand the content of the passage.Students' reading achievements are typically evaluated by the target ability as well as a secondary ability to understand the content of the passage.Students' reading achievements are typically evaluated by the target ability as well as a secondary ability to understand the content of the passage.</p>
+                    </div>
+                    <div class="feature-item">
+                        <img className="truong" src="../../public/img/trường.png"/>
+                        <p>Students' reading achievements are typically evaluated by the target ability as well as a secondary ability to understand the content of the passage.Students' reading achievements are typically evaluated by the target ability as well as a secondary ability to understand the content of the passage.Students' reading achievements are typically evaluated by the target ability as well as a secondary ability to understand the content of the passage.</p>
+                    </div>
+                </div> */}
+            </div>
+        </div>
+    );
+}
+export default Gioithieu;
