f468450fd015d53ef3d4f59cd8f463b7227d5262
nguyentr02 <nguyen.truong200226@vnuk.edu.vn>
2022-07-19 14:25:27 +0700

    New

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -4,48 +4,6 @@ import './NavbarElement.css';
 const Navbar = () => {
   return (
     <div class="Navbar">
-      {/* <div class="leftNav">
-        <a id="trang-chu-bt" class="btn" href="/trang-chu.js">Trang chủ</a>
-        <div class="dropdown">
-          <a class="dropbtn" href='/gioi-thieu.js'>Về VNUK</a>
-          <div class="dropdown-content">
-            <div className='dropdown-submenu'>
-
-              <a class="dropbtn1" href="/gioi-thieu.js">Giới thiệu</a>
-
-              <div class="submenu">
-                <a tabIndex={-1} href="#">Lịch sử phát triển</a>
-                <a tabIndex={-1} href="#">Tầm nhìn & sứ mệnh</a>
-                <a tabIndex={-1} href="#">Cơ cấu tổ chức</a>
-                <a tabIndex={-1} href="#">Thành tựu</a>
-                <a tabIndex={-1} href="#">Đối tác</a>
-                <a tabIndex={-1} href="#">Ba công khai</a>
-                <a tabIndex={-1} href="#">Liên hệ</a>
-                <a tabIndex={-1} href="#">Quà lưu niệm</a>
-              </div>
-            </div>
-            <div class="dropdown-content1">
-              <a href="#">Lịch sử phát triển</a>
-              <a href="#">Tầm nhìn & sứ mệnh</a>
-              <a href="#">Cơ cấu tổ chức</a>
-              <a href="#">Thành tựu</a>
-              <a href="#">Đối tác</a>
-              <a href="#">Ba công khai</a>
-              <a href="#">Liên hệ</a>
-              <a href="#">Quà lưu niệm</a>
-            </div>
-
-            <a href="#">Đào tạo</a>
-            <a href="#">Tuyển sinh</a>
-            <a href="#">Sinh viên</a>
-            <a href="#">Nghiên cứu</a>
-            <a href="#">Sáng kiến</a>
-            <a href="#">Tin tức & sự kiện</a>
-
-          </div>
-        </div>
-      </div> */}
-
       <div class='leftNav'>
         <ul>
           <li id="trang-chu-bt"><a href="/trang-chu.js">Trang chủ</a></li>
@@ -55,7 +13,6 @@ const Navbar = () => {
                 <li className='hoverme'><a href='/gioi-thieu.js'>Giới thiệu</a>
                   <div className='submenu2'>
                     <ul>
-                      {/* <li><a href='#'></a>Lịch sử phát triển</li> */}
                       <li><a href="#">Tầm nhìn & sứ mệnh</a>
                       </li>
                       <li><a href='#'>Lịch sử phát triển</a></li>
@@ -71,7 +28,20 @@ const Navbar = () => {
                 </li>
                 <li><a href="#">Đào tạo</a>
                 </li>
-                <li><a href="#">Tuyển sinh</a>
+                <li className='hoverme'><a href="#">Tuyển sinh</a>
+                  <div className='submenu2'>
+                    <ul>
+                      <li><a href="#">Xét tuyển vào VNUK</a>
+                      </li>
+                      <li><a href='#'>Nộp hồ sơ</a></li>
+                      <li><a href="#">Học phí, lệ phí</a>
+                      </li>
+                      <li><a href="#">Học bổng, hỗ trợ tài chính</a></li>
+                      <li><a href="#">Thăm quan VNUK</a></li>
+                      <li><a href="#">Liên hệ với sinh viên</a></li>
+                      <li><a href="/blogs.js">Blogs</a></li>
+                    </ul>
+                  </div>
                 </li>
                 <li><a href="#">Sinh viên</a></li>
                 <li><a href="#">Nghiên cứu</a></li>
@@ -83,7 +53,7 @@ const Navbar = () => {
         </ul>
       </div>
       <div className='rightNav'>
-        
+
       </div>
     </div>
   )
---
+++ src/pages/blogs.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './blogs.css'
+
+const blogs = () => {
+    return (
+        <div className='blogs'>
+            <div className='1field'>
+                <img src='public\img\blog-img1.jpg' alt='img'></img>
+            </div>
+        </div>
+    )
+}
+
+export default blogs;
