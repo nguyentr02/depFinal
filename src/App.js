0aad5a771d3bf339228b49ad77922b96cf528cea
nguyentr02 <nguyen.truong200226@vnuk.edu.vn>
2022-07-19 23:14:54 +0700

    Change in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar/index.js';
 import Gioithieu from './pages/gioi-thieu';
+import Footer from './components/Footer/footer.js';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Gioithieu />
+
+      <Footer />
     </div>
   );
 }
