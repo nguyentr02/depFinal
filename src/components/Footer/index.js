523293fc9585878932e24e4a88f2da29df295ff4
nguyentr02 <nguyen.truong200226@vnuk.edu.vn>
2022-07-19 13:37:59 +0700

    Re-edit index.html and footer in src/components/footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './NavbarElement.css';
+
+const footer = () => 
+{   return (
+        <div className='footer'>
+            
+        </div>
+)
+}
--- src/pages/trang-chu.js
+++ src/pages/trang-chu.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './trang-chu.css'
 
 const trangchu = () => {
     return (
