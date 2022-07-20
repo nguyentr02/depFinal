9200414865ac4852d2a4ead098590156b42abce3
DungNT0 <91143508+DungNT0@users.noreply.github.com>
2022-07-20 22:25:52 +0700

    add section 3 to trangchu

--- src/pages/trang-chu.js
+++ src/pages/trang-chu.js
@@ -3,51 +3,136 @@ import './trang-chu.css'
 
 const trangchu = () => {
     return (
-        <div id="section_5">
-            <div id="section_5_background">
-                <table>
-                    <thead id="section_5_thead">
-                        <th id="section_5_th1" class="section_5_th"> Đào tạo </th>
-                        <th class="section_5_th"> Giá trị quốc tế </th>
-                        <th class="section_5_th"> Đội ngũ </th>
-                    </thead>
+        <div className = "trangchu">
+            <div id="section_3">
+                <div id="books_group">
+                    <img id="books_icon" src="../assets/img/sách.png"/>
+                    <div class="section_3_text" id="books_text">
+                        <p id="books_header">Trung tâm giáo dục quốc tế</p>
+                        <p id="books_body">
+                        VNUK là trường đại học quốc tế <br/>
+                        công lập đầu tiên tại miền Trung <br/>
+                        Việt Nam.
+                        </p>
+                    </div>
+                </div>
 
-                    <tbody id="section_5_tbody">
-                        <tr>
-                            <td id="section_5_td_1">                                
-                                Chương trình đào tạo của VNUK được tư vấn về chuyên <br/>
-                                môn bởi các giáo sư của Đại học Aston, Vương quốc Anh <br/>
-                                - đối tác chiến lược của VNUK do Chính phủ Anh giới <br/>
-                                thiệu và giao nhiệm vụ hỗ trợ VNUK. Hiện Đại học Aston <br/>
-                                đang được xếp hạng top 500 trên cả hai bảng xếp hạng <br/>
-                                trường đại học của thế giới gồm QS Global World Ranking <br/>
-                                và Time Higher Education (THE) 2022.                                
-                            </td>
+                <div id="globe_group">
+                    <img id="globe_icon" src="../assets/img/cầu.png"/>
+                    <div class="section_3_text" id="globe_text">
+                        <div id="globe_header">Đáp ứng tiêu chuẩn đào tạo</div>
+                        <div id="globe_body">
+                        Chương trình gắn kết đào tạo với <br/>
+                        nhu cầu thực tế của doanh nghiệp <br/>
+                        và xu hướng phát triển của thị <br/>
+                        trường lao động quốc tế
+                        </div>
+                    </div>
+                </div>
 
-                            <td id="section_5_td_2">                                
-                                Chất lượng chương trình đào tạo của VNUK có giá trị quốc <br/>
-                                tế bởi được chấp nhận bởi các trường đại học uy tín thế <br/>
-                                giới qua chương trình chuyển tiếp 2+2, 3+1 và được công <br/>
-                                nhận bởi các Hiệp hội nghề nghiệp quốc tế như Hiệp hội <br/>
-                                kế toán công chứng Anh quốc (ACCA), Hiệp hội về Lãnh <br/>
-                                đạo và quản lí (CMI), Hiệp hội về Marketing được công <br/>
-                                nhận bởi Hoàng gia Anh (CIM), Học viện AWS (Amazon <br/>
-                                Web Service Academy).                               
-                            </td>
+                <div id="degree_group">
+                    <img id="degree_icon" src="../assets/img/bằng.png"/>
+                    <div class="section_3_text" id="degree_text">
+                        <div id="degree_header">Đại học và Sau Đại học</div>
+                        <div id="degree_body">
+                        VNUK hiện có 6 chương trình đào <br/>
+                        tạo đại học và 1 chương trình đào <br/>
+                        tạo Sau Đại học
+                        </div>
+                    </div>
+                </div>
 
-                            <td id="section_5_td_3">                               
-                                Bằng sự tâm huyết đối với giáo dục cũng như tình yêu đối    <br/>
-                                với thế hệ trẻ, đội ngũ giảng viên được đào tạo bài bản tại <br/>
-                                các nền giáo dục tiên tiến trên thế giới tại VNUK đang      <br/>  
-                                không ngừng nỗ lực để nâng cao hơn nữa chất lượng đào       <br/>      
-                                tạo và đem đến cho sinh viên một hành trình đại học thật    <br/>       
-                                sự giá trị và nhiều hứng khởi.                                
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+                <div id="school_group">
+                    <img id="school_icon" src="../assets/img/trường.png"/>
+                    <div class="section_3_text" id="school_text">
+                        <div id="school_header">Chất lượng sinh viên</div>
+                        <div id="school_body">
+                        Chất lượng sinh viên sau khi tốt <br/>
+                        nghiệp là trọng tâm hàng đầu của <br/>
+                        đội ngũ cán bộ giảng viên VNUK 
+                        </div>
+                    </div>
+                </div>
+
+                <div id="section_3_border"></div>
+
+                <div id="link_group">
+                    <div id="link_header_group">
+                        <img id="link_icon" src="../assets/img/link.png"/>
+                        <div class="section_3_text" id="link_header">Truy cập nhanh</div>
+                    </div>
+
+                    <table id="link_table">
+                        <tbody class="section_3_text">
+                            <tr class="link_table_row" id="ltr1">
+                                <td class="link_table_data_1">Dự bị đại học</td>
+                                <td class="link_table_data_2">Quản trị và Kinh doanh quốc tế</td>
+                                <td class="link_table_data_3">Quản trị Kinh doanh Số</td>
+                            </tr>
+
+                            <tr class="link_table_row" id="ltr2">
+                                <td class="link_table_data_1">Quản trị Du lịch và Khách sạn quốc tế</td>
+                                <td class="link_table_data_2">Phân tích Marketing</td>
+                                <td class="link_table_data_3">Khoa học và Kỹ thuật máy tính</td>
+                            </tr>
+
+                            <tr class="link_table_row" id="ltr3">
+                                <td class="link_table_data_1">Khoa học Dữ liệu</td>
+                                <td class="link_table_data_2">Khoa học Y sinh</td>
+                                <td class="link_table_data_3">Công nghệ Nano</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+            <div id="section_5">
+                <div id="section_5_background">
+                    <table>
+                        <thead id="section_5_thead">
+                            <th id="section_5_th1" class="section_5_th"> Đào tạo </th>
+                            <th class="section_5_th"> Giá trị quốc tế </th>
+                            <th class="section_5_th"> Đội ngũ </th>
+                        </thead>
+
+                        <tbody id="section_5_tbody">
+                            <tr>
+                                <td id="section_5_td_1">                                
+                                    Chương trình đào tạo của VNUK được tư vấn về chuyên <br/>
+                                    môn bởi các giáo sư của Đại học Aston, Vương quốc Anh <br/>
+                                    - đối tác chiến lược của VNUK do Chính phủ Anh giới <br/>
+                                    thiệu và giao nhiệm vụ hỗ trợ VNUK. Hiện Đại học Aston <br/>
+                                    đang được xếp hạng top 500 trên cả hai bảng xếp hạng <br/>
+                                    trường đại học của thế giới gồm QS Global World Ranking <br/>
+                                    và Time Higher Education (THE) 2022.                                
+                                </td>
+
+                                <td id="section_5_td_2">                                
+                                    Chất lượng chương trình đào tạo của VNUK có giá trị quốc <br/>
+                                    tế bởi được chấp nhận bởi các trường đại học uy tín thế <br/>
+                                    giới qua chương trình chuyển tiếp 2+2, 3+1 và được công <br/>
+                                    nhận bởi các Hiệp hội nghề nghiệp quốc tế như Hiệp hội <br/>
+                                    kế toán công chứng Anh quốc (ACCA), Hiệp hội về Lãnh <br/>
+                                    đạo và quản lí (CMI), Hiệp hội về Marketing được công <br/>
+                                    nhận bởi Hoàng gia Anh (CIM), Học viện AWS (Amazon <br/>
+                                    Web Service Academy).                               
+                                </td>
+
+                                <td id="section_5_td_3">                               
+                                    Bằng sự tâm huyết đối với giáo dục cũng như tình yêu đối    <br/>
+                                    với thế hệ trẻ, đội ngũ giảng viên được đào tạo bài bản tại <br/>
+                                    các nền giáo dục tiên tiến trên thế giới tại VNUK đang      <br/>  
+                                    không ngừng nỗ lực để nâng cao hơn nữa chất lượng đào       <br/>      
+                                    tạo và đem đến cho sinh viên một hành trình đại học thật    <br/>       
+                                    sự giá trị và nhiều hứng khởi.                                
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
+        
     )
 }
 
