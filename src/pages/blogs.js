839339f16916cea66e57b713058f2d9cab82f516
nguyentr02 <nguyen.truong200226@vnuk.edu.vn>
2022-07-22 20:29:52 +0700

    Edit blog Top image field

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -5,8 +5,60 @@ const blogs = () => {
     return (
         <div className='blogs'>
             <div className='imgField'>
-                <img id='fImg' src={require('../assets/img/blog-img1.jpg')} alt='img' />
-                
+                <h1 id='blogs'>Blogs</h1>
+            </div>
+            <div className='secondField'>
+                <div className='container'>
+                    <div className='title'>
+                        ĐĂNG KÝ XÉT TUYỂN
+                    </div>
+                    <div className='sub_title'>
+                        Kỳ tuyển sinh năm học 2022-2023
+                    </div>
+                    <form action='#'>
+                        <div className='details'>
+                            <div className='input_box'>
+                                <span className='details'>Tiêu đề</span>
+                                <input type='text' placeholder=""></input>
+                            </div>
+                            <div className='input_box'>
+                                <span className='details'>Họ và tên</span>
+                                <input type='text' placeholder=""></input>
+                            </div>
+                            <div className='input_box'>
+                                <span className='details'>Email</span>
+                                <input type='text' placeholder=""></input>
+                            </div>
+                            <div className='input_box'>
+                                <span className='details'>Số điện thoại</span>
+                                <input type='text' placeholder=""></input>
+                            </div>
+                            <div className='input_box'>
+                                <span className='details'>Thành phố đang sinh sống</span>
+                                <input type='text' placeholder=""></input>
+                            </div>
+                            <div className='input_box'>
+                                <span className='details'>Nơi đăng ký xét tuyển</span>
+                                <input type='text' placeholder=""></input>
+                            </div>
+                            <div className='input_box'>
+                                <span className='details'>Chuyên nghành</span>
+                                <input type='text' placeholder=""></input>
+                            </div>
+                            <div className='input_box'>
+                                <span className='details'>Bạn biết đến VNUK từ đâu</span>
+                                <input type='text' placeholder=""></input>
+                            </div>
+                            <div className='input_box'>
+                                <span className='details'>Lời nhắn</span>
+                                <input type='text' placeholder=""></input>
+                            </div>
+                            <div className='button'>
+                                <input type='submit' value="Đăng ký"></input>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     )
