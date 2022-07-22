75221402e129d2dc2f169349f411f83d72223724
DungNT0 <91143508+DungNT0@users.noreply.github.com>
2022-07-22 20:22:12 +0700

    add vnuk history js and css file

---
+++ src/pages/vnuk_founding_history.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './vnuk_fh.css'
+
+const vnukfh = () => {
+    return (
+        <div className = "vnukfh">
+
+        </div>
+        
+    )
+}
+
+
+export default vnukfh;
