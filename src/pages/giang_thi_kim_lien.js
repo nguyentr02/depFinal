34701201bc27031357e2a8c87b3f176a376998b6
DungNT0 <91143508+DungNT0@users.noreply.github.com>
2022-07-24 11:13:21 +0700

    add giang_thi_kim_lien js and css files

---
+++ src/pages/giang_thi_kim_lien.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './giang_thi_kim_lien.css'
+
+const giangthikimlien = () => {
+    return (
+        <div className = "giang_thi_kim_lien">
+            
+        </div>
+        
+    )
+}
+
+
+export default giangthikimlien;
