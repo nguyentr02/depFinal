c7efdbb6c7d3d85437ea4e5d3bc95be2c9cd2ec3
DungNT0 <91143508+DungNT0@users.noreply.github.com>
2022-07-24 22:31:39 +0700

    add vision images, js and css files

---
+++ src/pages/vision.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './vision.css'
+
+const vision = () => {
+    return (
+        <div className = "vision">
+            
+        </div>
+        
+    )
+}
+
+
+export default vision;
