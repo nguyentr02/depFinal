7b926f5bf274194ea3a59563a3dc440d205c8a1a
DungNT0 <91143508+DungNT0@users.noreply.github.com>
2022-07-25 18:27:17 +0700

    add recruit images,js and css files

---
+++ src/pages/recruit.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './recruit.css'
+
+const Recruit = () => {
+    return (
+        <div className = "Recruit">
+            
+        </div>
+        
+    )
+}
+
+
+export default Recruit;
