fdb02930ed7752f553ab8d9eb8591f1a2f950491
DungNT0 <91143508+DungNT0@users.noreply.github.com>
2022-07-27 09:13:37 +0700

    add ex_student images, js and css files

---
+++ src/pages/ex_student.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './ex_student.css'
+
+const ExStudent = () => {
+    return (
+        <div className = "ExStudent">
+            
+        </div>
+        
+    )
+}
+
+
+export default ExStudent;
