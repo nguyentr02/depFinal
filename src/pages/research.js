f049ec6a736376de5361f356dc10971dfb29d572
DungNT0 <91143508+DungNT0@users.noreply.github.com>
2022-07-27 19:04:06 +0700

    add research images, js and css files, and updated ex_student css file

---
+++ src/pages/research.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './research.css'
+
+const Research = () => {
+    return (
+        <div className = "Research">
+            
+        </div>
+        
+    )
+}
+
+
+export default Research;
