34cb6170c785e9d6bd247a49aad852f23352e40b
nguyentr02 <nguyen.truong200226@vnuk.edu.vn>
2022-08-01 16:30:04 +0700

    change root in App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
-import './App.css';
-import Navbar from './components/Navbar/index.js';
-import Gioithieu from './pages/gioi-thieu';
-import Trangchu from './pages/trang-chu.js';
-import Vision from './pages/vision';
-import VnukFh from './pages/vnuk_founding_history';
-import GiangThiKimLien from './pages/giang_thi_kim_lien';
-import ExStudent from './pages/ex_student';
-import Recruit from './pages/recruit';
-import Research from './pages/research';
-import Footer from './components/Footer/footer.js';
-
-import ToChuc from './pages/tochuc';
-import Huong from './pages/huong';
+import "./App.css";
+import Navbar from "./components/Navbar/index.js";
+import Gioithieu from "./pages/gioi-thieu.js";
+import Trangchu from "./pages/trang-chu.js";
+import Vision from "./pages/vision";
+import VnukFh from "./pages/vnuk_founding_history";
+import GiangThiKimLien from "./pages/giang_thi_kim_lien";
+import ExStudent from "./pages/ex_student";
+import Recruit from "./pages/recruit";
+import Research from "./pages/research";
+import Footer from "./components/Footer/footer.js";
+import Blogs from "./pages/blogs";
+import ToChuc from "./pages/tochuc";
+import Huong from "./pages/huong";
+import { BrowserRouter,Routes, Route, } from "react-router-dom";
 
 function App() {
   return (
-    
-    <div className='App'>
-      
-
-      <Navbar/>
-      <ToChuc/>
-      <Footer/>
-      
+    <div className="App">
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Trangchu />} />
+        <Route path="/gioithieu" exact element={<Gioithieu />} />
+        <Route path="/vision" exact element={<Vision />} />
+        <Route path="/vnukfh" exact element={<VnukFh />} />
+        <Route path="/giangthikimlien" element={<GiangThiKimLien />} />
+        <Route path="/exstudent" exact element={<ExStudent />} />
+        <Route path="/recruit" exact element={<Recruit />} />
+        <Route path="/research" exact element={<Research />} />
+        <Route path="/blogs" exact element={<Blogs />} />
+        <Route path="/tochuc" exact element={<ToChuc />} />
+        <Route path="/huong" exact element={<Huong />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
     </div>
   );
 }
