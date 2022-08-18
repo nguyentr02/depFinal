0144b2ab2f804e82659dcec4220c901509b80b7d
nguyentr02 <nguyen.truong200226@vnuk.edu.vn>
2022-08-18 14:20:07 +0700

    Edit visit page

---
+++ src/pages/visit.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './visit.css';
+
+const Visit = () => {
+    return (
+        <div className="visit">
+            <div className="visit_1Field">
+                <h1 id="visit_1Field-text">Tham quan trường</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Visit;
